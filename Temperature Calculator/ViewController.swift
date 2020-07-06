1e27aab161ab8e0c5d7fd49a64d53d8fd63b8847
Benjamin Inemugha <bennyblasco@gmail.com>
2020-07-06 21:04:46 +0100

    Initial Commit

---
+++ Temperature Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Temperature Calculator
+//
+//  Created by Benjamin Inemugha on 06/07/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
